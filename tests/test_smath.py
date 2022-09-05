0c346fabbccd71d1ad6c5c68f3b0a61e9d92e9e3
andrewmelnyk <melnyk.andrew@gmail.com>
2022-09-04 21:19:32 -0400

    adding more tests and flask app

---
+++ tests/test_smath.py
@@ -0,0 +1,14 @@
+from re import sub
+from smath import subtract
+
+def setup_function(function):
+    print(f" Running Setup: {function.__name__}");
+    function.x = 10
+
+def teardown_function(function):
+    print(f" Running Teardown: {function.__name__}")
+    del function.x
+
+def test_hello_subtract():
+    f = test_hello_subtract.x
+    assert subtract(9) == 8
---
+++ tests/test_web.py
@@ -0,0 +1,22 @@
+import pytest
+import json
+
+from web import app as flask_app
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_index(app, client):
+    result = client.get("/")
+    assert result.status_code == 200
+    assert {"hello": "bob"} == json.loads(result.get_data(as_text=True))
+
+def test_change(app, client):
+    result = client.get("/change/10/1")
+    assert result.status_code == 200
+    assert [{"40": "quarters"},{"1": "dimes"}] == json.loads(result.get_data(as_text=True))
